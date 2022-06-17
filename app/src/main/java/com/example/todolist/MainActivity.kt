e604947814ebb8b0491e51fed510a0089e4268d7
SIRIKONDA-AKHILA <90668579+AKHILA-SW10@users.noreply.github.com>
2022-06-17 15:22:01 -0500

    verify changed

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -7,7 +7,7 @@ import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
-//verify
+//verify changed
 class MainActivity : AppCompatActivity() {
     private lateinit var todoAdapter: ToDoAdapter
 
